3b843b5b7cc5cff590e685ca5a304e54ccd824eb
melzreal <melissa.real@hotmail.com>
2021-11-28 17:21:29 +0000

    Add custom s3 back

--- melfolio/settings/production.py
+++ melfolio/settings/production.py
@@ -6,7 +6,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 BASE_URL = os.environ.get('BASE_URL')
