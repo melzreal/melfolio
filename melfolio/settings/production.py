aa45a0cd570beeb6273a6189f716d238f980b19c
melzreal <melissa.real@hotmail.com>
2021-11-28 16:52:02 +0000

    Add allowed hosts

--- melfolio/settings/production.py
+++ melfolio/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -8,6 +8,10 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+BASE_URL = os.environ.get('BASE_URL')
+LIVE_URL = os.environ.get('LIVE_URL')
+ALLOWED_HOSTS = [BASE_URL, LIVE_URL]
 
 try:
     from .local import *
