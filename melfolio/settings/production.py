126955992a6330fd876fa652805bb3595096f21f
melzreal <melissa.real@hotmail.com>
2021-11-28 16:43:43 +0000

    Remove customdomain for now

--- melfolio/settings/production.py
+++ melfolio/settings/production.py
@@ -6,9 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')
 
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 try:
